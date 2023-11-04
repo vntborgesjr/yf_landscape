648a4e03d05c92cb0671bbd052f9723a758ce0cc
Vitor Borges Jr <vntborgesjr@gmail.com>
2023-11-04 13:13:25 -0300

    Create my_hist_facet.R

---
+++ R/my_hist_facet.R
@@ -0,0 +1,51 @@
+my_hist_facet <- function(
+    data,
+    x,
+    binwidth,
+    xlab,
+    ylab
+) {
+  
+  #
+  data |> 
+    ggplot2::ggplot() +
+    ggplot2::aes(x = {{ x }}) +
+    ggplot2::geom_histogram(
+      fill = "#bdd005",
+      color = "black",
+      binwidth = binwidth,
+      center = binwidth/2
+    ) +
+    ggplot2::labs(
+      x = xlab,
+      y = ylab
+    ) +
+    ggplot2::facet_wrap(
+      facets = ggplot2::vars(
+        buffer
+      ), 
+      scales = "free_y"
+    ) +
+    ggplot2::theme_classic(
+      base_size = 14,
+      base_family = "serif"
+    ) +
+    ggplot2::theme(
+      text = ggplot2::element_text(
+        color = "black", 
+        face = "bold"
+      ),
+      axis.text.x = ggplot2::element_text(
+        size = 12
+      ),
+      axis.text.y = ggplot2::element_text(size = 12),
+      # axis.ticks.x = ggplot2::element_blank(),
+      strip.background = ggplot2::element_rect(
+        color = "white"
+      ), 
+      strip.text = ggplot2::element_text(
+        color = "black"
+      )
+    )
+  
+}
