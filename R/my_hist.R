b6c08c135e06383649b5d7df6558d3da9a0c3a62
Vitor Borges Jr <vntborgesjr@gmail.com>
2023-11-30 15:36:39 -0300

    Update my_hist.R

--- R/my_hist.R
+++ R/my_hist.R
@@ -3,7 +3,17 @@ my_hist <- function(
     x,
     binwidth,
     xlab,
-    ylab
+    ylab,
+    fill = "#bdd005",
+    xfrom,
+    xto,
+    xby,
+    yfrom,
+    yto,
+    yby,
+    text_size = 18,
+    axis_size = 14,
+    face = NULL
 ) {
   
   #
@@ -11,7 +21,7 @@ my_hist <- function(
     ggplot2::ggplot() +
     ggplot2::aes(x = {{ x }}) +
     ggplot2::geom_histogram(
-      fill = "#bdd005",
+      fill = fill,
       color = "black",
       binwidth = binwidth,
       center = binwidth/2
@@ -20,19 +30,21 @@ my_hist <- function(
       x = xlab,
       y = ylab
     ) +
+    ggplot2::scale_x_continuous(breaks = seq(xfrom, xto, xby)) +
+    ggplot2::scale_y_continuous(breaks = seq(yfrom, yto, yby)) +
     ggplot2::theme_classic(
-      base_size = 14,
+      base_size = text_size,
       base_family = "serif"
     ) +
     ggplot2::theme(
       text = ggplot2::element_text(
         color = "black", 
-        face = "bold"
+        face = face
       ),
       axis.text.x = ggplot2::element_text(
-        size = 12
+        size = axis_size
       ),
-      axis.text.y = ggplot2::element_text(size = 12),
+      axis.text.y = ggplot2::element_text(size = axis_size),
       # axis.ticks.x = ggplot2::element_blank(),
       strip.background = ggplot2::element_rect(
         color = "white"
