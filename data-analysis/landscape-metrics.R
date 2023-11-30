ea138dbaddd75f2ef6d9c6ba7d807d57b7c78b2b
Vitor Borges Jr <vntborgesjr@gmail.com>
2023-11-30 15:35:36 -0300

    Update landscape-metrics.R

--- data-analysis/landscape-metrics.R
+++ data-analysis/landscape-metrics.R
@@ -7,12 +7,15 @@
 #                                             #
 ##%#########################################%##
 
+# The objectives of the script are to calculate three landscape metrics ─ edge
+# density, number of patches and habitat amount ─ inside local landscapes 
+# represented by seven concentric, circular buffer areas of increase radii 
+# (100, 200, 400, 800, 1000, 2000, and 5000 m) around each epizootic event and 
+# to create a wide data frame containing all epizootic events and metrics
+
 # Load data -------------------------------------------
 source(here::here("data/landscape-spatial-data.R"))
 # cities_cover <- raster::raster(here::here("data/cities-cover.tif"))
-
-# Load packages -------------------------------------------
-source(here::here("R/dependencies.R"))
 
 # Landscape metrics -------------------------------------------
 # View(list_lsm("class"))
@@ -33,7 +36,7 @@ metrics <- c(
 # buffer of 40m -------------------------------------------
 landscape_metrics40 <- buffer40 |> 
   purrr::map(
-    \(.x) calculate_lsm(
+    \(.x) landscapemetrics::calculate_lsm(
       landscape = .x,
       what = metrics
     )
@@ -45,7 +48,7 @@ landscape_metrics40 <- buffer40 |>
 # Buffer of 100m
 landscape_metrics100 <- buffer100 |> 
   purrr::map(
-    \(.x) calculate_lsm(
+    \(.x) landscapemetrics::calculate_lsm(
       landscape = .x,
       what = metrics
     )
@@ -57,7 +60,7 @@ landscape_metrics100 <- buffer100 |>
 # Buffer of 200m
 landscape_metrics200 <- buffer200 |> 
   purrr::map(
-    \(.x) calculate_lsm(
+    \(.x) landscapemetrics::calculate_lsm(
       landscape = .x,
       what = metrics
     )
@@ -69,7 +72,7 @@ landscape_metrics200 <- buffer200 |>
 # Buffer of 400m
 landscape_metrics400 <- buffer400 |> 
   purrr::map(
-    \(.x) calculate_lsm(
+    \(.x) landscapemetrics::calculate_lsm(
       landscape = .x,
       what = metrics
     )
@@ -81,7 +84,7 @@ landscape_metrics400 <- buffer400 |>
 # Buffer of 1000m
 landscape_metrics1000 <- buffer1000 |> 
   purrr::map(
-    \(.x) calculate_lsm(
+    \(.x) landscapemetrics::calculate_lsm(
       landscape = .x,
       what = metrics
     )
@@ -93,7 +96,7 @@ landscape_metrics1000 <- buffer1000 |>
 # Buffer of 2000m
 landscape_metrics2000 <- buffer2000 |> 
   purrr::map(
-    \(.x) calculate_lsm(
+    \(.x) landscapemetrics::calculate_lsm(
       landscape = .x,
       what = metrics, 
     )
@@ -105,7 +108,7 @@ landscape_metrics2000 <- buffer2000 |>
 # Buffer of 5000m
 landscape_metrics5000 <- buffer5000 |> 
   purrr::map(
-    \(.x) calculate_lsm(
+    \(.x) landscapemetrics::calculate_lsm(
       landscape = .x,
       what = metrics
     )
@@ -160,3 +163,97 @@ ep_landscape_metrics <- landscape_metrics_list |>
   ) |> 
   dplyr::select(4:32)
 
+# data transformation - standardize (z-transformation) --------------------
+
+ep_landscape_metrics_transformed <- ep_landscape_metrics |> 
+  dplyr::mutate(
+    status = dplyr::if_else(
+      status == "Negative",
+      true = 0,
+      false = 1
+    ),
+    ed40_ztransformed = vegan::decostand(
+      x = ed40,
+      method = "standardize"
+    ),
+    ed100_ztransformed = vegan::decostand(
+      x = ed100,
+      method = "standardize"
+    ),
+    ed200_ztransformed = vegan::decostand(
+      x = ed200,
+      method = "standardize"
+    ),
+    ed400_ztransformed = vegan::decostand(
+      x = ed400,
+      method = "standardize"
+    ),
+    ed1000_ztransformed = vegan::decostand(
+      x = ed1000,
+      method = "standardize"
+    ),
+    ed2000_ztransformed = vegan::decostand(
+      x = ed2000,
+      method = "standardize"
+    ),
+    ed5000_ztransformed = vegan::decostand(
+      x = ed5000,
+      method = "standardize"
+    ),
+    np40_ztransformed = vegan::decostand(
+      x = np40,
+      method = "standardize"
+    ),
+    np100_ztransformed = vegan::decostand(
+      x = np100,
+      method = "standardize"
+    ),
+    np200_ztransformed = vegan::decostand(
+      x = np200,
+      method = "standardize"
+    ),
+    np400_ztransformed = vegan::decostand(
+      x = np400,
+      method = "standardize"
+    ),
+    np1000_ztransformed = vegan::decostand(
+      x = np1000,
+      method = "standardize"
+    ),
+    np2000_ztransformed = vegan::decostand(
+      x = np2000,
+      method = "standardize"
+    ),
+    np5000_ztransformed = vegan::decostand(
+      x = np5000,
+      method = "standardize"
+    ),
+    pland40_ztransformed = vegan::decostand(
+      x = pland40,
+      method = "standardize"
+    ),
+    pland100_ztransformed = vegan::decostand(
+      x = pland100,
+      method = "standardize"
+    ),
+    pland200_ztransformed = vegan::decostand(
+      x = pland200,
+      method = "standardize"
+    ),
+    pland400_ztransformed = vegan::decostand(
+      x = pland400,
+      method = "standardize"
+    ),
+    pland1000_ztransformed = vegan::decostand(
+      x = pland1000,
+      method = "standardize"
+    ),
+    pland2000_ztransformed = vegan::decostand(
+      x = pland2000,
+      method = "standardize"
+    ),
+    pland5000_ztransformed = vegan::decostand(
+      x = pland5000,
+      method = "standardize"
+    )
+  )
