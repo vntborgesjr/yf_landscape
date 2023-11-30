c49807e7ab6a7d224779ea6b6ac1ac4d6101f1b0
Vitor Borges Jr <vntborgesjr@gmail.com>
2023-11-30 15:36:36 -0300

    Create model_adjustment_selection.R

---
+++ R/model_adjustment_selection.R
@@ -0,0 +1,49 @@
+model_adjustment_selection <- function(data,
+                                models,
+                                FUN,
+                                family,
+                                rank = NULL,
+                                beta = "none") {
+  # load packages
+  library(tibble)
+  library(MuMIn)
+  
+  # create the object to receivve the models
+  models_results <- list()
+  
+  # fit the set of models
+  models_results <- models |> 
+    purrr::map(
+      \(.x) glmmTMB::glmmTMB(
+        formula = .x,
+        data = data,
+        family = family
+      )
+    )
+    
+  # run model selection
+  m_selection <- MuMIn::model.sel(models_results,
+                           beta = beta) 
+  
+  # extradct model names for the first column of model selection table
+  
+  model_names <- list() # create the object to store the name of models
+  
+  for (i in seq_along(models)) {
+    model_names[[i]] <- formula(models[[i]])
+  } # extract and store model names
+  
+  
+  # generate model selection table
+  
+  m_sel_tab <- tibble::tibble(Modelo = as.character(model_names[as.double(row.names(m_selection))]),
+                      gl = m_selection$df,
+                      logLik = round(m_selection$logLik, 2),
+                      AICc = round(m_selection$AICc, 2),
+                      deltaAICc = round(m_selection$delta, 2),
+                      wi = round(m_selection$weight, 3)) 
+  
+  return(list(models = models_results,
+              model_selection = m_selection, 
+              model_selection_table = m_sel_tab))
+}
